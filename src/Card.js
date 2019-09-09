75d5c7db8af0454c0e7d4c13c2d5942ac6f6a90d
Karolina Poslek <karolina@poslek.com>
2019-09-09 11:22:18 +0200

    Creating properties

--- src/Card.js
+++ src/Card.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
 
-class Card extends Component {
-    render() {
-        return (
-            <div className="tc br2 ba green bg-washed-red dib br3 pa3 ma2 b--hot-pink mv4 w-100 w-50-m w-25-l mw5 grow shadow-5">
-                <img
-                    alt="cats"
-                    className="w-70"
-                    src="https://robohash.org/kitty?set=set4"
-                />
-                <h2 className="dt w-100 mt1">Cute Cats</h2>
-            </div>
-        );
-    }
-}
+const Card = props => {
+    return (
+        <div className="tc br2 ba green bg-washed-red dib br3 pa3 ma2 b--hot-pink mv4 w-100 w-50-m w-25-l mw5 grow shadow-5">
+            <img
+                alt="cats"
+                className="w-70"
+                src={`https://robohash.org/kitty${props.id}?set=set4`}
+            />
+            <h2 className="dt w-100 mt1">{props.name}</h2>
+            <p>{props.email}</p>
+        </div>
+    );
+};
 
 export default Card;
--- src/index.js
+++ src/index.js
@@ -4,12 +4,25 @@ import './index.css';
 import Card from './Card';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
+import { cuteCats } from './CuteCats';
 
 ReactDOM.render(
     <div>
-        <Card />
-        <Card />
-        <Card />
+        <Card
+            id={cuteCats[0].id}
+            name={cuteCats[0].name}
+            email={cuteCats[0].email}
+        />
+        <Card
+            id={cuteCats[1].id}
+            name={cuteCats[1].name}
+            email={cuteCats[1].email}
+        />
+        <Card
+            id={cuteCats[2].id}
+            name={cuteCats[2].name}
+            email={cuteCats[2].email}
+        />
     </div>,
     document.getElementById('root')
 );
