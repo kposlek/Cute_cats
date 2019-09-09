f330b0f105b355a4ef31e6c20eb5ed724cae2314
Karolina Poslek <karolina@poslek.com>
2019-09-09 10:31:35 +0200

    Creating Card Component

---
+++ src/Card.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Card extends Component {
+    render() {
+        return (
+            <div className="tc br2 ba green bg-washed-red dib br3 pa3 ma2 b--hot-pink mv4 w-100 w-50-m w-25-l mw5 grow shadow-5">
+                <img
+                    alt="cats"
+                    className="w-70"
+                    src="https://robohash.org/kitty?set=set4"
+                />
+                <h1 className="dt w-100 mt1">Cute Cats</h1>
+            </div>
+        );
+    }
+}
+
+export default Card;
